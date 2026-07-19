43e3ecfe5e916f03fbca6494bf6311b75b9bdc6a
Urooj Fatima <uroojanees2007@gmail.com>
2026-07-19 15:14:12 +0500

    Add files via upload

---
+++ SumOfTwoNo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SumOfTwoNo {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        float a = scanner.nextFloat();
+        float b = scanner.nextFloat();
+        float result = a + b;
+        System.out.println("The sum is: " + result);
+    }
+}
